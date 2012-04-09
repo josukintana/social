a49cbd8af94ed4b1cdcc80b58599d13b7afc6695
isaacjarquin <graciosillo21@yahoo.es>
2012-04-09 21:50:24 +0100

    Refactoring groups management

--- lib/social/groups_management.rb
+++ lib/social/groups_management.rb
@@ -7,29 +7,17 @@ module GroupsManagement
   
   module InstanceMethods
     def group_exists?(name)
-      if (self.groups.find_by_name(name).nil?)
-        return false
-      else
-        return true
-      end
+      !self.groups.find_by_name(name).nil?
     end
     
     def add_group(name)
-      if (!group_exists?(name))
-        self.groups.create(:name => name)
-        return "The group '" + name + "' has been correctly created."
-      else
-        return "The group '" + name + "' already exists."  
-      end
+      self.groups.create(:name => name) if (!group_exists?(name))
+      !group_exists?(name) ? "The group '" + name + "' has been correctly created." : "The group '" + name + "' already exists."
     end
     
     def remove_group(name)
-      if (group_exists?(name))
-        self.groups.find_by_name(name).destroy
-        return "The group '" + name + "' has been correctly removed."
-      else
-        return "The group '" + name + "' doesn't exist."
-      end
+      self.groups.find_by_name(name).destroy if group_exists?(name)
+      group_exists?(name) ? "The group '" + name + "' has been correctly removed." : "The group '" + name + "' doesn't exist."
     end
   end
  
